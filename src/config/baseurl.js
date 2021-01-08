4f3f02b47f7113dccdf1d9e0d21fba1f61acc10e
Anna Malik <anna.malik@fis.agh.edu.pl>
2021-01-08 10:45:04 +0100

    changed baseurl

--- src/config/baseurl.js
+++ src/config/baseurl.js
@@ -1,3 +1,3 @@
 module.exports = {
-    apiBaseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080"
+    apiBaseURL: process.env.REACT_APP_API_BASE_URL || "https://star-wars-service.herokuapp.com"
 };
